26cd8395f60eb40f62ae3e37de229f8f3e94b3b0
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-11 01:52:22 +0600

    initial setup complete

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+app.get( '/', (req, res) => {
+    res.send('Student Connection Portal is running')
+} )
+
+
+app.listen( port, () => {
+    console.log(`Portal is running on port: ${port}`);
+} )
