78d0741c905f69051c373ef08ff66427a1ab42fd
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-12 06:33:35 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
 const app = express();
@@ -10,14 +11,41 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.begblt8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
 
 
 
-app.get( '/', (req, res) => {
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Student Connection Portal is running')
-} )
+})
 
 
-app.listen( port, () => {
+app.listen(port, () => {
     console.log(`Portal is running on port: ${port}`);
-} )
+})
