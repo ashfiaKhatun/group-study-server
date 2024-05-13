b9a9be8fc9ffeef7fb4fb3c402dbadca8903494a
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-13 06:54:55 +0600

    post and get method connected with mongodb

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5000'],
+    credentials: true
+}));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next();
+})
+
 app.use(express.json());
 
 
@@ -25,7 +39,18 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
+        const assignmentCollection = client.db("assignment_collection").collection("assignments");
 
+        app.get('/all-assignments', async (req, res) => {
+            const result = await assignmentCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/all-assignments', async (req, res) => {
+            const newAssignment = req.body;
+            const result = await assignmentCollection.insertOne(newAssignment);
+            res.send(result);
+        })
 
 
 
